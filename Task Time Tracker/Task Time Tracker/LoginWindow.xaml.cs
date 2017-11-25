3d0627997fcd462e03cb32f953ced10e60e62e10
Ruben Aghajanyan <ruboo1993@gmail.com>
2017-11-26 01:14:47 +0400

    WPF project initial setup.

---
+++ Task Time Tracker/Task Time Tracker/LoginWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Task_Time_Tracker.Utility_Functions;
+
+namespace Task_Time_Tracker
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        CRM_Connector crmConnector;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string userName = LoginBox.Text;
+            string password = PasswordBox.Password;
+
+            if (userName == "" || password == "")
+                MessageBox.Show("Please provide your CRM credentials.");
+            else
+            {
+                crmConnector = new CRM_Connector(userName, password, "https://bever.bever.am/XRMServices/2011/Organization.svc");
+
+                Tuple<string, string> connectionStatus = crmConnector.Connect_To_MSCRM();
+
+                if (connectionStatus.Item1 != "0")
+                    MessageBox.Show(connectionStatus.Item2);
+                else
+                    MessageBox.Show("Successfully Connected!");
+
+
+            }
+        }
+    }
+}
---
+++ Task Time Tracker/Task Time Tracker/Utility Functions/CRM_connector.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel.Description;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_Time_Tracker.Utility_Functions
+{
+    class CRM_Connector
+    {
+        private string username;
+        private string password;
+        private string soap_uri;
+
+        public IOrganizationService service;
+
+        public CRM_Connector(string Username, string Password, string Soap_Service_URI)
+        {
+            username = Username;
+            password = Password;
+            soap_uri = Soap_Service_URI;
+        }
+
+        public Tuple<string, string> Connect_To_MSCRM()
+        {
+
+            string connection_code = "0";
+            string error_message = "";
+
+
+            try
+            {
+                ClientCredentials credentials = new ClientCredentials();
+                credentials.UserName.UserName = username;
+                credentials.UserName.Password = password;
+                Uri serviceUri = new Uri(soap_uri);
+                using (OrganizationServiceProxy proxy = new OrganizationServiceProxy(serviceUri, null, credentials, null))
+                {
+
+                    proxy.EnableProxyTypes();
+                    proxy.Authenticate();
+                    proxy.Timeout = new TimeSpan(0, 30, 0);
+                    service = (IOrganizationService)proxy;
+                    //  _proxy=proxy;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                connection_code = "-1";
+                error_message = ex.Message;
+            }
+
+            return new Tuple<string, string>(connection_code, error_message);
+        }
+    }
+}
