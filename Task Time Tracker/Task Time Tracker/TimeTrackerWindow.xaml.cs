62b226ce59c74750ab83907da96904b3a3dd0d74
Arsen Aghajanyan <aghajanyanarsen@gmail.com>
2018-11-30 15:51:49 +0400

    Preparing for Squirrel.

--- Task Time Tracker/Task Time Tracker/TimeTrackerWindow.xaml.cs
+++ Task Time Tracker/Task Time Tracker/TimeTrackerWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Squirrel;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
@@ -47,6 +48,16 @@ namespace Task_Time_Tracker
             RetrieveUserName();
 
             SetIdleTimer();
+
+            CheckForUpdates();
+        }
+
+        private async Task CheckForUpdates()
+        {
+            using (var updateManager = UpdateManager.GitHubUpdateManager("https://github.com/BeverCRM/TimeTrackingApp"))
+            {
+                await updateManager.Result.UpdateApp();
+            }
         }
 
         private void SetIdleTimer()
